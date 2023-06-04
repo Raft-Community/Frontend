0a611b1f069a9e138f7c1924dfc30474405627fc
chino <839203521@qq.com>
2023-06-05 03:51:57 +0800

    feat: home layout

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,13 +1,57 @@
-import Head from "next/head";
+function PostCard() {
+  return (
+    <div className="card lg:card-side bg-base-100 shadow-xl my-2">
+      <figure><img src="/images/stock/photo-1494232410401-ad00d5433cfa.jpg" alt="Album"/></figure>
+      <div className="card-body">
+        <h2 className="card-title">New album is released!</h2>
+        <p>Click the button to listen on Spotiwhy app.</p>
+        <div className="card-actions justify-end">
+          <button className="btn btn-primary">Listen</button>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Home</title>
-      </Head>
-
-      <div>contents</div>
+      <div className="h-full w-full bg-gray-100 flex flex-row">
+        <div className="h-full w-2/3 flex flex-col">
+          <div className="h-1/3 w-full flex flex-col">
+            <div className="h-1/6 w-full flex items-center border-2">
+              <h1 className="text-xl pl-2">Raft Community</h1>
+            </div>
+            <div className="h-5/6 w-full p-4 justify-center border-2">
+              <textarea className="textarea textarea-primary h-5/6 w-full" placeholder="Input your idea here..."></textarea>
+              <div className="h-1/6 w-full flex items-center justify-end">
+                <button className="btn btn-primary btn-sm">Send</button>
+              </div>
+            </div>
+          </div>
+          <div className="h-2/3 w-full flex flex-col p-6 pt-8 overflow-scroll">
+            <PostCard />
+            <PostCard />
+            <PostCard />
+          </div>
+        </div>
+        <div className="h-full w-1/3 grid grid-rows-3 grid-flow-col gap-4">
+          <div className="w-full bg-gray-300 flex justify-center items-center">
+            <div>
+              <p>IP: 127.0.0.1</p>
+              <p>
+                Port: 2333
+              </p>
+            </div>
+          </div>
+          <div className="w-full bg-gray-300 flex justify-center items-center">
+            Member List
+          </div>
+          <div className="w-full bg-gray-300 flex justify-center items-center">
+            System Info
+          </div>
+        </div>
+      </div>
     </>
   );
 }
--- app/instances/page.tsx
+++ app/instances/page.tsx
@@ -1,54 +1,54 @@
 'use client';
 
-import Head from 'next/head';
 import Link from 'next/link';
 
-function InstanceCard({
-  name,
-  id,
-}: {
-  name: string;
-  id: string;
-}) {
+function InstanceCard({ name, id }: { name: string; id: string }) {
   return (
-      <div className="card bg-gray-400 shadow-xl">
-        <Link href={`/home?id=${id}`} className='h-full'>
-          <div className="card-body">
-            <h1 className="card-title">{name}</h1>
-            <p className="card-text">Instance id: {id}</p>
-            <p className="card-text">IP: 127.0.0.1</p>
-            <p className="card-text">Port: {Math.floor(Math.random() * 9000) + 1000}</p>
-          </div>
-        </Link>
-      </div>
+    <div className="card bg-gray-300 shadow-xl">
+      <Link href={`/home?id=${id}`} className="h-full">
+        <div className="card-body">
+          <h1 className="card-title">{name}</h1>
+          <p className="card-text">Instance id: {id}</p>
+          <p className="card-text">IP: 127.0.0.1</p>
+          <p className="card-text">
+            Port: 2333
+          </p>
+        </div>
+      </Link>
+    </div>
   );
 }
 
 function AddInstance() {
   return (
-    <div className="card bg-gray-400 shadow-xl flex justify-center items-center">
-      <button className="btn" onClick={()=>(window as any).addInstance.showModal()}>Add an instance</button>
+    <div className="card flex justify-center items-center">
+      <button
+        className="btn bg-blue-300"
+        onClick={() => (window as any).addInstance.showModal()}
+      >
+        Add an instance
+      </button>
       <dialog id="addInstance" className="modal">
         <form method="dialog" className="modal-box">
-          <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+          <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+            ✕
+          </button>
           <h3 className="font-bold text-lg">Add an Instance Here</h3>
           <p className="py-4">Comming soon...</p>
           <div className="modal-action">
-            <button className="btn" type="submit">Submit</button>
+            <button className="btn" type="submit">
+              Submit
+            </button>
           </div>
         </form>
       </dialog>
     </div>
-);
+  );
 }
 
 export default function Instances() {
   return (
     <>
-      <Head>
-        <title>Instances</title>
-      </Head>
-      
       <div className="card h-4/5 w-4/5 bg-gray-100 shadow-xl">
         <div className="card-body">
           <h1 className="card-title">Instances</h1>
@@ -63,7 +63,6 @@ export default function Instances() {
           </div>
         </div>
       </div>
-      
     </>
   );
 }
